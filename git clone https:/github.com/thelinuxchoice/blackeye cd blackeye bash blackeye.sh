3a1e26078ad2918be83969e540acf096b4e75dad
Reijaz <52032654+Reijaz@users.noreply.github.com>
2021-06-26 11:16:14 +0300

    Create blackeye cd blackeye bash blackeye.sh

---
+++ git clone https:/github.com/thelinuxchoice/blackeye cd blackeye bash blackeye.sh
@@ -0,0 +1,11 @@
+apt update
+apt upgrade
+pkg instal git
+pkg installpython
+pkg install curl
+pkg install php
+pkg install openssl
+pkg install wget
+git clone https://github.com/thelinuxchoice/blackeye
+cd blackeye
+bash blackeye.sh
